26700de513f635579187d45dc6021619b1c54516
m.mozafari <m.mozafari@DEVELOPER-10>
2023-02-21 10:59:24 +0330

    add canactivate

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,20 +1,35 @@
-import 'rxjs/add/operator/map';
-
-import { CanActivate, Router } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuardService implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {}
-  CanActivate() {
-    this.auth.user$.map((user) => {
-      if (user) return true;
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.auth.user$.pipe(
+      map((user) => {
+        if (user) return true;
 
-      this.router.navigate(['/login']);
-      return false;
-    });
+        this.router.navigate(['/login']);
+        return false;
+      })
+    );
   }
 }
